1b54bdf034b7965d2b667d594493093583659af1
Sreemayee-Bachu <sreemayee77@gmail.com>
2023-05-01 20:48:40 +0530

    Sample1

--- sprint2/src/main/java/com/sprint2/controller/LoginControl.java
+++ sprint2/src/main/java/com/sprint2/controller/LoginControl.java
@@ -1,5 +1,6 @@
 package com.sprint2.controller;
 import java.util.*;
+//branch2 change
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
